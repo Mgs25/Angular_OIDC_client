01e3b554761ae2e17fc2332658cdfb18dd4086d3
Mgs25 <methran2003@gmail.com>
2022-09-12 02:07:09 +0530

    OIDC flow with refresh token

--- projects/sample-code-flow-auth0/src/app/auth-config.module.ts
+++ projects/sample-code-flow-auth0/src/app/auth-config.module.ts
@@ -5,18 +5,19 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        authority: 'https://dev-damienbod.eu.auth0.com',
+        // authority: 'https://dev-damienbod.eu.auth0.com',
+        authority: 'https://dev-od3saiv7.us.auth0.com',
         redirectUrl: window.location.origin,
         postLogoutRedirectUri: window.location.origin,
-        clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
+        clientId: 'rgB8qeaJNIdSf0qDMAY2LHNqPQFVHyHO',
         scope: 'openid profile offline_access auth0-user-api-spa',
         responseType: 'code',
         silentRenew: true,
         useRefreshToken: true,
         logLevel: LogLevel.Debug,
-        customParamsAuthRequest: {
-          audience: 'https://auth0-api-spa',
-        },
+        // customParamsAuthRequest: {
+        //   audience: 'https://auth0-api-spa',
+        // },
         customParamsRefreshTokenRequest: {
           scope: 'openid profile offline_access auth0-user-api-spa',
         },
